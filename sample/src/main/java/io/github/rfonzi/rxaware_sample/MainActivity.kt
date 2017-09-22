6dedc5192353780153e0a9622c9c4f1b0dea2ae7
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-09-22 03:52:59 -0400

    Add puck

--- sample/src/main/java/io/github/rfonzi/rxaware_sample/MainActivity.kt
+++ sample/src/main/java/io/github/rfonzi/rxaware_sample/MainActivity.kt
@@ -1,12 +1,48 @@
 package io.github.rfonzi.rxaware_sample
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.ImageButton
+import com.jakewharton.rxbinding2.view.clicks
 import io.github.rfonzi.rxaware.BaseActivity
+import io.reactivex.Observable
 
 class MainActivity : BaseActivity() {
+
+    lateinit var vm: MainViewModel
+    lateinit var puck: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vm = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        puck = findViewById(R.id.puck)
+
+        vm.exposePuck(puck.clicks())
+
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        val dimens = Observable.fromCallable { metrics.widthPixels to metrics.heightPixels }
+
+
+        vm.exposeDimens(dimens)
+
+        vm.coordinates()
+                .subscribe {
+                    puck.x = it.first.toFloat()
+                    puck.y = it.second.toFloat()
+                }
+                .lifecycleAware()
+
+        vm.winSignal()
+                .subscribe {
+                    puck.visibility = View.GONE
+                }
+                .lifecycleAware()
     }
+
+
 }
--- sample/src/main/java/io/github/rfonzi/rxaware_sample/MainViewModel.kt
+++ sample/src/main/java/io/github/rfonzi/rxaware_sample/MainViewModel.kt
@@ -1,15 +1,63 @@
 package io.github.rfonzi.rxaware_sample
 
 import io.github.rfonzi.rxaware.BaseViewModel
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 import java.util.*
 
 /**
  * Created by ryan on 9/22/17.
  */
+
 class MainViewModel : BaseViewModel() {
 
-    val random = Random(Calendar.DATE.toLong())
+    private val random = Random(Calendar.DATE.toLong())
+    private var onTop = true
+
+    var score = 0
+    val maxScore: Long = 1000
+    private var height = 0
+    private var width = 0
 
 
+    private val coordinates: BehaviorSubject<Pair<Int, Int>> = BehaviorSubject.create()
+    private val win: BehaviorSubject<Boolean> = BehaviorSubject.create()
+
+    fun coordinates(): Observable<Pair<Int, Int>> = coordinates
+    fun winSignal(): Observable<Boolean> = win
+
+
+
+    fun exposePuck(clicks: Observable<Unit>) = clicks
+            .subscribe {
+                coordinates.onNext(nextCoords())
+                onTop = !onTop
+            }
+            .lifecycleAware()
+
+    fun exposeDimens(dimens: Observable<Pair<Int, Int>>) = dimens
+            .subscribe {
+                width = it.first
+                height = it.second
+                score++
+                if (score >= maxScore)
+                    win.onNext(true)
+            }
+            .lifecycleAware()
+
+    private fun nextCoords(): Pair<Int, Int> {
+        var x = random.nextInt(width)
+        var y = random.nextInt(height)
+
+        if (height > width)
+            y /= 2
+        else
+            x /= 2
+
+        if(!onTop)
+            y += height.div(2)
+
+        return x to y
+    }
 
 }
