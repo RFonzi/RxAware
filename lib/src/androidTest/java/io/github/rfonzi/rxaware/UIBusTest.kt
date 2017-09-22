2b4a3ebad09f60095ed61863a8e024981c1eba57
Ryan Fonzi <ryan.fonzi@gmail.com>
2017-09-22 15:40:29 -0400

    Add UIBus Tests

---
+++ lib/src/androidTest/java/io/github/rfonzi/rxaware/UIBusTest.kt
@@ -0,0 +1,44 @@
+package io.github.rfonzi.rxaware
+
+import android.support.test.runner.AndroidJUnit4
+import io.github.rfonzi.rxaware.bus.UIBus
+import io.github.rfonzi.rxaware.bus.events.Event
+import io.github.rfonzi.rxaware.bus.events.NavigateUpEvent
+import io.github.rfonzi.rxaware.bus.events.ToastEvent
+import io.reactivex.observers.TestObserver
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Created by ryan on 9/22/17.
+ */
+
+@RunWith(AndroidJUnit4::class)
+class UIBusTest {
+
+    @Test
+    fun testSendingToast() {
+        val expected = "testing"
+
+        val testObserver = TestObserver<Event>()
+        UIBus.toObservable()
+                .subscribe(testObserver)
+        UIBus.toast(expected)
+
+        testObserver.assertValue(ToastEvent(expected))
+
+    }
+
+    @Test
+    fun testSendingNavigateUp() {
+        val testObserver = TestObserver<Event>()
+        UIBus.toObservable()
+                .subscribe(testObserver)
+        UIBus.navigateUp()
+
+        testObserver.assertValue { it is NavigateUpEvent }
+    }
+
+}
